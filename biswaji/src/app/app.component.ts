2662c5b4616cdfb1bf49707f34490907f98b5b78
biswajiit <biswajit.jmishra@gmail.com>
2018-08-12 19:55:01 +0530

    commit

--- biswaji/src/app/app.component.ts
+++ biswaji/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'biswaji';
+  detail:String = "Senior Software developer.";
+  name:String = "Biswajit Mishra";
+  gitLink:String = "https://github.com/biswajiit";
+  linkedInLink:String ="https://www.linkedin.com/in/biswajit-mishra-55060867/";
+  linkedInLink_short:String = "https://bit.ly/2v67CaB";
 }
--- biswaji/src/app/app.module.ts
+++ biswaji/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { BlogService } from './services/blog.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    BlogService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
